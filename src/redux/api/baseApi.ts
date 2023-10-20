4efcc0d023c00c7044eb6329dc556b78abf97fc8
sifatullahsu <personal.sifat@gmail.com>
2023-10-20 12:23:09 +0600

    IMPORTANT: server url change.

--- src/redux/api/baseApi.ts
+++ src/redux/api/baseApi.ts
@@ -4,7 +4,7 @@ import { tagsArray } from '../tags'
 
 export const baseApi = createApi({
   reducerPath: 'api',
-  baseQuery: axiosBaseQuery({ baseUrl: 'http://localhost:5000/api/v1' }),
+  baseQuery: axiosBaseQuery({ baseUrl: `https://mentor-plus-server.vercel.app/api/v1` }),
   endpoints: () => ({}),
   tagTypes: tagsArray
 })
