df704c1e37989a53e95f747c47b2644c44877feb
sifatullahsu <personal.sifat@gmail.com>
2023-11-03 08:21:48 +0600

    base api change

--- src/redux/api/baseApi.ts
+++ src/redux/api/baseApi.ts
@@ -2,12 +2,12 @@ import { createApi } from '@reduxjs/toolkit/query/react'
 import { axiosBaseQuery } from '../axios/axiosBaseQuery'
 import { tagsArray } from '../tags'
 
-// const liveLink = `https://mentor-plus-server.vercel.app/api/v1`
-const localLink = `http://localhost:5000/api/v1`
+const server = `https://mentor-plus-server.vercel.app/api/v1`
+// const server = `http://localhost:5000/api/v1`
 
 export const baseApi = createApi({
   reducerPath: 'api',
-  baseQuery: axiosBaseQuery({ baseUrl: localLink }),
+  baseQuery: axiosBaseQuery({ baseUrl: server }),
   endpoints: () => ({}),
   tagTypes: tagsArray
 })
